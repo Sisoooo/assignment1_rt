2b8db327d17d008c7ea86b1a5b51f2ca8069de9e
Francesco <f.sisani2002@gmail.com>
2025-11-28 09:48:54 +0000

    Removed build, install and log folders

--- build/assignment1_rt/ament_cmake_python/assignment1_rt/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='assignment1_rt',
-    version='0.0.0',
-    packages=find_packages(
-        include=('assignment1_rt', 'assignment1_rt.*')),
-)
